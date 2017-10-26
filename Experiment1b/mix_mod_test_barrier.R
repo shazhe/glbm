6942687399d8ac49728c1d29ae89511d18a3aa0a
shazhe <zhesha1006@gmail.com>
2017-10-26 17:42:07 +0100

    barrier test done and now globe

--- Experiment1b/mix_mod_test_barrier.R
+++
@@ -1,8 +0,0 @@
-####################################################################################
-#### This file demonstrate 3 update method for dealing with mixture processes
-#### Use the prototype model and code from Bakka's Barrier model in INLA
-#### The update methods are
-#### 1. Model as a single stationary process and mask the land with desired values
-#### 2. Model two stationary processes and seam them together
-#### 3. Joint mixture model
-####################################################################################
