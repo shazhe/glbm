598fdc5f91efc9aaced8c166725c922accbfca0e
shazhe <zhesha1006@gmail.com>
2017-11-13 11:32:54 +0000

    new structure for experiment 1b

---
+++ Experiment1b/GIA_Rmd/functions.R
@@ -0,0 +1,216 @@
+## 1 print plots
+my.dev.print <- function(path, filename, format = c("pdf", "pgn")){
+  if(format == "png"){
+    fname <- paste0(path, filename, ".png") 
+    dev.print(png, fname, width = par("din")[1], height = par("din")[2], 
+              units = "in", pointsize = 12, res = 72, bg = "transparent")
+  }else if(format == "pdf"){
+    fname <- paste0(path, filename, ".pdf") 
+    dev.print(pdf, fname)
+  }
+  
+}
+
+
+## 2 Transforming priors from original space to log-normal space
+Tlognorm <- function(mu, v){
+  logv <- log(1 + v/mu^2)
+  logmu <- log(mu^2) - 0.5*log(mu^2 + v)
+  return(c(logmu, logv))
+}
+
+## 3 Generate Fibonacci points on the sphere
+fiboSphere <- function(N = 1000L, LL = TRUE, L0 = FALSE) {
+  ## Reference (note that points generated from 2D are slightly different from 3D)
+  ## Measurement of Areas on a Sphere Using Fibonacci and Latitude–Longitude Lattices (2010)
+  phi <- (sqrt(5) + 1) / 2  # golden ratio
+  if(LL){ ## for lonlat coords
+    i <- seq(-N, N)
+    P <- 2 * N + 1
+    lat <- asin(2*i / P) * 180 / pi
+    if(L0){ ## for lon in [0, 360]
+      lon <- ((2 * pi * i / phi) %% pi) * 360 / pi
+    }else{ ## for lon in [-180, 180]
+      lon <- ((2 * pi * i / phi) %% pi) * 360 / pi - 180
+    }
+    return(cbind(lon = lon, lat = lat))
+  }
+  else{ ## for xyz coords
+    i <- seq(-(N-1), (N-1),2)
+    theta <- 2 * pi * i / phi
+    sphi <- i/N
+    cphi <- sqrt((N+i) * (N-i))/N
+    
+    x <- cphi * sin(theta)
+    y <- cphi * cos(theta)
+    z <- sphi
+    return(cbind(x,y,z))
+  }
+}
+
+## 4 Wrapper for esitmation and prediction using INLA  
+BayesDA_GIA <- function(GIA, GPS, trho, tsigma){
+  
+  fibo_points <- fiboSphere2(N = 12960)
+  mesh_points_xyz <- do.call(cbind, Lll2xyz(lat = fibo_points[,2], lon = fibo_points[,1]))
+  Mesh_GIA <- inla.mesh.2d(loc = mesh_points_xyz, cutoff = 0.01, max.edge = 0.5)
+  
+  ## Transform the GIA data
+  polycoords <- GIA[,c(6:13, 6,7)] 
+  plist <- lapply(GIA$ID, function(x) Polygons(list(Polygon(cbind(lon = as.numeric(polycoords[x, c(1,3,5,7,9)]), 
+                                                                  lat = as.numeric(polycoords[x, c(2,4,6,8,10)])))), ID = x))
+  Plist <- SpatialPolygons(plist, proj4string = CRS("+proj=longlat"))
+  ## Convert the Cartesian to longlat with long in [0, 360)
+  MlocLL <- Lxyz2ll(list(x=Mesh_GIA$loc[,1], y = Mesh_GIA$loc[,2], z = Mesh_GIA$loc[,3]))
+  MlocLL$lon <- ifelse(MlocLL$lon <0, MlocLL$lon + 359, MlocLL$lon)
+  M_sp <- SpatialPoints(data.frame(lon = MlocLL$lon, lat = MlocLL$lat), proj4string = CRS("+proj=longlat")) 
+  Midx <- over(M_sp, Plist)
+  GIA_prior <- GIA$trend[Midx]
+  
+  ## Transform the GPS data
+  GPS_loc <- do.call(cbind, Lll2xyz(lat = GPS$lat, lon = GPS$lon))
+  GPSX <- ifelse(GPS$lon < 0, GPS$lon + 359, GPS$lon)
+  GPS_sp <- SpatialPoints(data.frame(lon = GPSX, lat = GPS$lat), proj4string = CRS("+proj=longlat"))
+  Midx2 <- over(GPS_sp, Plist)
+  GPS_mu <- GIA$trend[Midx2]
+  GPS_data <- GPS$trend - GPS_mu
+  
+  ## The SPDE model
+  lsigma0 <- tsigma[1]
+  theta1_s <- tsigma[2]
+  lrho0 <- trho[1]
+  theta2_s <- trho[2]
+  lkappa0 <- log(8)/2 - lrho0
+  ltau0 <- 0.5*log(1/(4*pi)) - lsigma0 - lkappa0
+  GIA_spde <- inla.spde2.matern(Mesh_GIA, B.tau = matrix(c(ltau0, -1, 1),1,3), B.kappa = matrix(c(lkappa0, 0, -1), 1,3),
+                                theta.prior.mean = c(0,0), theta.prior.prec = c(sqrt(1/theta1_s), sqrt(1/theta2_s)))
+  
+  ## Link the process to observations and predictions
+  A_data <- inla.spde.make.A(mesh = Mesh_GIA, loc = GPS_loc)
+  A_pred <- inla.spde.make.A(mesh = Mesh_GIA, loc = rbind(GPS_loc, Mesh_GIA$loc))
+  
+  ## Create the estimation and prediction stack
+  st.est <- inla.stack(data = list(y=GPS_data), A = list(A_data),
+                       effects = list(GIA = 1:GIA_spde$n.spde), tag = "est")
+  st.pred <- inla.stack(data = list(y=NA), A = list(A_pred),
+                        effects = list(GIA=1:GIA_spde$n.spde), tag = "pred")
+  stGIA <- inla.stack(st.est, st.pred)
+  
+  ## Fix the GPS errors
+  hyper <- list(prec = list(fixed = TRUE, initial = 0))
+  formula = y ~ -1 +  f(GIA, model = GIA_spde)
+  prec_scale <- c(1/GPS$std^2, rep(1, nrow(A_pred)))
+  
+  ## Run INLA
+  res_inla <- inla(formula, data = inla.stack.data(stGIA, spde = GIA_spde), family = "gaussian",
+                   scale =prec_scale, control.family = list(hyper = hyper),
+                   control.predictor=list(A=inla.stack.A(stGIA), compute =TRUE))
+  INLA_pred <- res_inla$summary.linear.predictor
+  
+  ## Extract and project predictions
+  pred_idx <- inla.stack.index(stGIA, tag = "pred")$data
+  GPS_idx <- pred_idx[1:nrow(GPS)]
+  GIA_idx <- pred_idx[-c(1:nrow(GPS))]
+  
+  ## GPS 
+  GPS_u <- INLA_pred$sd[GPS_idx]
+  GPS_pred <- data.frame(lon = GPSX, lat = GPS$lat, u = GPS_u)
+  
+  ## GIA
+  GIA_diff <- INLA_pred$mean[GIA_idx] 
+  GIA_m <- GIA_diff + GIA_prior
+  GIA_u <- INLA_pred$sd[GIA_idx]
+  proj <- inla.mesh.projector(Mesh_GIA, projection = "longlat", dims = c(360,180), xlim = c(0, 360), ylim = c(-90, 90))
+  GIA_grid <- expand.grid(proj$x, proj$y)
+  GIA_pred <- data.frame(lon = GIA_grid[,1], lat = GIA_grid[,2],
+                         diff = as.vector(inla.mesh.project(proj, as.vector(GIA_diff))),
+                         mean = as.vector(inla.mesh.project(proj, as.vector(GIA_m))),
+                         u = as.vector(inla.mesh.project(proj, as.vector(GIA_u))))
+  
+  return(list(res_inla = res_inla, spde = GIA_spde, st = stGIA, 
+              mesh = Mesh_GIA, GPS_pred = GPS_pred, GIA_pred = GIA_pred))
+}
+
+
+## 5 Plot predicted mean
+map_res <- function(data, xname, yname, fillvar, colpal, limits=NULL, title){
+  plot_data <- data[c(xname, yname, fillvar)]
+  names(plot_data) <- c("X", "Y", "Fill")
+  
+  beauty <- 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          panel.background = element_rect(fill = "white", colour = 'white'), 
+          legend.text = element_text(size = 10),
+          legend.title = element_text(size = 10), 
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10),
+          axis.line = element_line(size = 1),
+          plot.title = element_text(hjust = 0.5, size = 15),
+          plot.subtitle = element_text(hjust = 0.5, size = 10),
+          panel.border = element_blank())
+  world_map <- map_data("world2")
+  baseworld <- geom_polygon(data = world_map, aes(x=long, y=lat, group=group), colour="grey", fill = NA, alpha = 0.5)  
+  
+  colbar <- guide_colorbar(barwidth = 2, barheight = 10, label.position = "right", title.position = "bottom")
+  
+  Map <- ggplot(plot_data) + geom_raster(aes(x = X, y = Y, fill = Fill)) + coord_fixed() + 
+    xlab("Longitude") + ylab("Latitude") + 
+    scale_x_continuous(limits=c(0,360),  expand = c(0, 0)) + 
+    scale_y_continuous(limits=c(-90,90),  expand = c(0, 0)) + 
+    scale_fill_gradientn(colors = colpal, name = "mm/yr", limits = limits,  guide = colbar) 
+  
+  Map <- Map +  baseworld + ggtitle(title) + beauty
+  return(Map)
+}
+
+## 6 Plot zoom in
+map_zoom <-function(data_field, data_obs, zoom_coords, colpal, limits=NULL){
+  lon1 <- zoom_coords$lon[1]
+  lon2 <- zoom_coords$lon[2]
+  lat1 <- zoom_coords$lat[1]
+  lat2 <- zoom_coords$lat[2]
+  
+  beauty <- 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          panel.background = element_rect(fill = "white", colour = 'white'), 
+          legend.text = element_text(size = 10),
+          legend.title = element_text(size = 10), 
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10),
+          axis.line = element_line(size = 1),
+          plot.title = element_text(hjust = 0.5, size = 15),
+          plot.subtitle = element_text(hjust = 0.5, size = 10),
+          panel.border = element_blank())
+  world_map <- map_data("world2")
+  baseworld <- geom_polygon(data = world_map, aes(x=long, y=lat, group=group), colour="grey", fill = NA, alpha = 0.5) + beauty 
+  
+  colbar <- guide_colorbar(barwidth = 2, barheight = 10, label.position = "right", title.position = "bottom")
+  
+  zoom_data <- subset(data_field, lon > lon1 & lon < lon2 & lat > lat1 & lat < lat2)
+  zoom_GPS <- subset(data_obs, lon > lon1 & lon < lon2 & lat > lat1 & lat < lat2)
+  
+  map_zoom <- ggplot(data=zoom_data) + geom_raster(aes(x = lon, y = lat, fill = u)) + 
+    coord_fixed() + xlab("Longitude") + ylab("Latitude") + 
+    scale_x_continuous(limits=c(lon1,lon2),  expand = c(0.03, 0.03)) + 
+    scale_y_continuous(limits=c(lat1,lat2),  expand = c(0, 0)) +
+    scale_fill_gradientn(colors = colpal, name = "mm/yr", guide = colbar) 
+  
+  zoom_std <- subset(zoom_data, lon %in% seq(lon1, lon2, 2))
+  zoom_std <- subset(zoom_std, lat %in% seq(lat1, lat2, 2))
+  
+  map_zoom <- map_zoom + geom_point(data=zoom_std, aes(x=lon, y=lat), pch=19, size = zoom_std$u,
+                                    col = "blue", fill = "blue", alpha=0.6) 
+  map_zoom <- map_zoom + geom_point(data=zoom_GPS, aes(x=lon, y=lat), pch= "*", size = 10,
+                                    col = "red", alpha = 0.8) 
+  
+  ## Calculate the approximate correlation length
+  cor_deg <- round(rho_mode /(2*pi) * 360)
+  cor_dist <- round(rho_mode * 6371)
+  
+  map_zoom <- map_zoom + baseworld  + beauty + 
+    ggtitle("GIA uncertainty Zoom-in", 
+            subtitle = paste("The estimated correlation length is about", eval(cor_dist), "km", "(", eval(cor_deg),"degree)"))
+  
+}
+
+
---
+++ Experiment1b/GIA_Rmd/loadprep.R
@@ -0,0 +1,21 @@
+#### This file load the GIA prior and GPS data and prepare the data for the BHM model
+
+#### 1 Load GIA prior
+if(Sys.info()["sysname"] == "Windows"){
+  ice6g <- read.table("Z:/WP2-SolidEarth/BHMinputs/GIA/GIA_Pel-6-VM5.txt", header = T)
+}else if(Sys.info()["sysname"] == "Ubuntu"){
+  ice6g <- read.table("~/globalmass/WP2-SolidEarth/GIAforwardModels/textfiles/GIA_Pel-6-VM5.txt", header = T)
+}else{
+  ice6g <- read.table("/./projects/GlobalMass/WP2-SolidEarth/GIAforwardModels/textfiles/GIA_Pel-6-VM5.txt", header = T)
+}
+
+#### 1 Load GPS data
+
+if(Sys.info()["sysname"] == "Windows"){
+  GPSV4b <- read.table("Z:/WP2-SolidEarth/BHMinputs/GPS/GPS_v04b.txt", header = T)
+}else if(Sys.info()["sysname"] == "Ubuntu"){
+  GPSV4b <- read.table("~/globalmass/WP2-SolidEarth/GPS/NGL/BHMinputFiles/GPS_v04b.txt", header = T)
+}else{
+  GPSV4b <- read.table("/./projects/GlobalMass/WP2-SolidEarth/GPS/NGL/BHMinputFiles/GPS_v04b.txt", header = T)
+}
+
