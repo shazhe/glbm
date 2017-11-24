b9aafe7689541b5bb10393eb8dd8f5bf449190dc
shazhe <zhesha1006@gmail.com>
2017-11-24 10:45:23 +0000

    change mesh resolution

--- Experiment2/Rscript/Exp2a.R
+++ Experiment2/Rscript/Exp2a.R
@@ -7,28 +7,6 @@ library(INLA)
 source("~/glbm/BHM_sphere/functions.R")
 source("~/glbm/BHM_sphere/functions-barriers-dt-models-march2017.R")
 
-## ----ssh_mesh------------------------------------------------------------
-## Load the Ocean polygon
-Ocean <- readOGR(dsn = "/./projects/GlobalMass/WP1-BHM/maps/ne_110m_ocean", layer = "ne_110m_ocean")
-plot(Ocean)
-
-## Genereate Fibonacci points on the sphere
-fibo_points <- fiboSphere(N = 660, L0 = TRUE)
-fibo_points_xyz <- do.call(cbind, Lll2xyz(lat = fibo_points[,2], lon = fibo_points[,1]))
-mesh0 <- inla.mesh.2d(loc = fibo_points_xyz, cutoff = 0.01, max.edge = 0.5)
-
-## Remove mesh not in teh ocean
-mesh0 <- dt.mesh.addon.posTri(mesh = mesh0, globe = TRUE)
-Tlonlat <- Lxyz2ll(list(x = mesh0$posTri[,1], y = mesh0$posTri[,2], z = mesh0$posTri[,3]))
-mesh0$Trill <- cbind(lon = Tlonlat$lon, lat =Tlonlat$lat)
-TinOcean <- unlist(over(Ocean, SpatialPoints(coords=mesh0$Trill, proj4string = CRS(proj4string(Ocean))), returnList=T))
-TAll <- 1:mesh0$t
-ToutOcean <- TAll[-TinOcean]
-Omega = dt.Omega(list(TinOcean, 1:mesh0$t), mesh0)
-
-mesh_ssh <- mesh.sub(mesh0, Omega, 1)
-summary(mesh_ssh)
-
 ## ----alt_data------------------------------------------------------------
 library(ncdf4)
 alt_nc <- nc_open("/./projects/GlobalMass/WP3-Ocean/BHMinputs/trend_SSH_CCI_200501_201512.nc")
@@ -61,13 +39,6 @@ lattice::levelplot(err_ssh ~ lon + lat, data = alt_data2, aspect = "iso",  at = 
                      },
                      main = title, xlab = "longitude", ylab = "latitude")
 
-## ----variogram, message = FALSE------------------------------------------
-library(gstat)
-coordinates(alt_data2) <- c("lon", "lat")
-proj4string(alt_data2) <- CRS("+proj=longlat")
-alt_datas <- alt_data2[sample(1:nrow(alt_data2), 5000),]
-v1 <- variogram(trend_ssh ~ 1, alt_datas) # Takes long time!
-plot(v1)
 
 ## ----prior---------------------------------------------------------------
 ## Priors mean and variance for the parameters: rho and sigma
@@ -76,6 +47,32 @@ v_r <- 1
 mu_s <- 5
 v_s <- 10^2
 
+## ----ssh_mesh------------------------------------------------------------
+## Genereate Fibonacci points on the sphere
+fibo_points <- fiboSphere(N = 12960, L0 = TRUE)
+fibo_points_xyz <- do.call(cbind, Lll2xyz(lat = fibo_points[,2], lon = fibo_points[,1]))
+mesh0 <- inla.mesh.2d(loc = fibo_points_xyz, cutoff = 0.01, max.edge = 1)
+## Make this "smoother"
+mesh0 <- inla.mesh.2d(loc = mesh0$loc, cutoff = 0.01, max.edge = 1)
+
+## ----ssh_mesh2-----------------------------------------------------------
+## Load the Ocean polygon
+Ocean <- readOGR(dsn = "/./projects/GlobalMass/WP1-BHM/maps/ne_110m_ocean", layer = "ne_110m_ocean")
+plot(Ocean)
+
+## Remove mesh not in the ocean
+mesh0 <- dt.mesh.addon.posTri(mesh = mesh0, globe = TRUE) 
+Tlonlat <- Lxyz2ll(list(x = mesh0$posTri[,1], y = mesh0$posTri[,2], z = mesh0$posTri[,3]))
+mesh0$Trill <- cbind(lon = Tlonlat$lon, lat =Tlonlat$lat)
+TinOcean <- unlist(over(Ocean, SpatialPoints(coords=mesh0$Trill, proj4string = CRS(proj4string(Ocean))), returnList=T))
+TAll <- 1:mesh0$t
+ToutOcean <- TAll[-TinOcean]
+Omega = dt.Omega(list(TinOcean, 1:mesh0$t), mesh0)
+
+mesh_ssh <- mesh.sub(mesh0, Omega, 1)
+summary(mesh_ssh)
+
+## ----spde----------------------------------------------------------------
 ## Transform the parameters for the SPDE_GMRF approximation
 trho <- Tlognorm(mu_r, v_r)
 tsigma <- Tlognorm(mu_s, v_s)
@@ -95,7 +92,7 @@ SSH_spde <- inla.spde2.matern(mesh_ssh, B.tau = matrix(c(ltau0, -1, 1),1,3), B.k
 ## ----link_data-----------------------------------------------------------
 ## Link the process to observations and predictions
 A_data <- inla.spde.make.A(mesh = mesh_ssh, loc = alt_loc)
-A_pred <- inla.spde.make.A(mesh = mesh_ssh, loc = mesh_ssh$loc)
+A_pred <- inla.spde.make.A(mesh = mesh_ssh, loc = rbind(mesh_ssh$loc))
 
 ## Create the estimation and prediction stack
 st.est <- inla.stack(data = list(y=alt_data2$trend_ssh), A = list(A_data),
@@ -117,5 +114,21 @@ res_inla <- inla(formula, data = inla.stack.data(stSSH, spde = SSH_spde), family
                  scale =prec_scale, control.family = list(hyper = hyper),
                  control.predictor=list(A=inla.stack.A(stSSH), compute =TRUE))
 
+## ----inla_res, include = TRUE, eval = FALSE------------------------------
 INLA_pred <- res_inla$summary.linear.predictor
+## Extract and project predictions
+pred_idx <- inla.stack.index(stSSH, tag = "pred")$data
 
+## SSH
+SSH_m <- INLA_pred$mean[pred_idx]
+SSH_u <- INLA_pred$sd[pred_idx]
+proj <- inla.mesh.projector(mesh_ssh, projection = "longlat", dims = c(360,180), xlim = c(0.5,359.5), ylim = c(-89.5, 89.5))
+SSH_grid <- expand.grid(proj$x, proj$y)
+SSH_pred <- data.frame(lon = SSH_grid[,1], lat = SSH_grid[,2],
+                       mean = as.vector(inla.mesh.project(proj, as.vector(SSH_m))),
+                       u = as.vector(inla.mesh.project(proj, as.vector(SSH_u))))
+
+ress <- list(res_inla = res_inla, spde = SSH_spde, st = stSSH,
+            mesh = mesh_ssh,  SSH_pred = SSH_pred)
+
+save(ress, file ="/./projects/GlobalMass/WP1-BHM/Experiment2a/exp2a_ssh.RData")
