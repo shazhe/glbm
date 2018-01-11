567f3314b446240dd3a7c2aeba09ad31274bbe1d
shazhe <zhesha1006@gmail.com>
2018-01-11 15:15:28 +0000

    change folder structure

--- Experiment1a/Rscript/02GP_MLE_update.R
+++
@@ -1,8 +0,0 @@
-###################################################################
-##      MLE (Empirical Bayesian) Gaussian Update                 ##
-## This script performs a Gaussian Update of the GIA processes   ##
-## Given                                                         ##
-## 1. MLE parameter values                                       ##
-## 2. subset of GPS obs                                          ##
-## 3. GIA forward model prior                                    ##
-###################################################################
